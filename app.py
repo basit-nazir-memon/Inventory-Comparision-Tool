aa61d4830483c05112d92fc10c3acbf4bdcefaf8
basit-nazir-memon <basitnazir585@gmail.com>
2025-03-05 14:16:10 +0500

    Added Backend Code

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, send_file
 import pandas as pd
 import io
+import os
 
 app = Flask(__name__)
 
@@ -41,4 +42,5 @@ def compare_inventory():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get PORT from Render, default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
